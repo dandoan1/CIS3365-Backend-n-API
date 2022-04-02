7f4f94b54f53a13be1fea7670399384d0b32f2a8
dandoan1 <72234672+dandoan1@users.noreply.github.com>
2022-04-02 13:11:30 -0500

    added add function and api

--- API Starter.py
+++ API Starter.py
@@ -3,7 +3,7 @@ import flask
 from flask import jsonify
 from flask import request, make_response
 from BackEndStarter import (email_signup_check, verification_token_checker, verification_token_maker,
-customer_signup_no_code, customer_signup_with_code, send_email)
+customer_signup_no_code, customer_signup_with_code, send_email, get_customer_info, get_all_tables, view_table, add_data)
 
 
 #Put all route ends here and put what they are example below
@@ -48,9 +48,8 @@ def api_user_create():
         if token == "":
             created_token = verification_token_maker()
             send_email(email, created_token)
-            #send_email_function_here(created_token)
+            print('token created', created_token)
         print('email check passed, sent email with token')
-        print('token created', created_token)
         if verification_token_checker(token):
             print('token check passed')
             if code == "":
@@ -63,30 +62,123 @@ def api_user_create():
                 return 'account has been created successfully'
         else:
             print('token check failed')
-            return 'token given was wrong'
+            return 'try entering your token again'
     else:
         return 'email existed'
 
+"""
+{
+    "firstname": "Dan",
+    "lastname": "ApiTesttoken",
+    "email": "thisthingsuck@gmail.com",
+    "code": "",
+    "token": "047R7U9SNF"
+}
+"""
 
-@app.route('/api/user/profile', methods=['POST'])
-def api_user_profile():
+
+
+
+
+@app.route('/api/user/profile/<email>', methods=['GET'])
+def api_customer_info(email):
+    info = get_customer_info(email)
+    return jsonify(info)
+
+#http://127.0.0.1:5000/api/user/profile/<email> for customer to view their information
+
+
+
+@app.route('/api/user/admin/table/get', methods=['GET'])
+def api_user_admin_get_table():
+    info = get_all_tables()
+    return jsonify(info)
+
+#go to link to get all tables name
+
+
+@app.route('/api/user/admin/table/view/<name>', methods=['GET'])
+def api_user_admin_view_table(name):
+    info = view_table(name)
+    results = []
+    for infos in info:
+        results.append(infos)
+    return jsonify(results)
+
+#enter specific table name to view all data inside such table
+
+@app.route('/api/user/admin/table/add', methods=['POST'])
+def api_user_admin_add_data():
     request_data = request.get_json() #requesting json
-    fname = request_data['firstname']
-    lname = request_data['lastname']
-    email = request_data['email']
-    code = request_data['code']
-    token = request_data['token']
+    table_name = request_data['table_name']
+    para1 = request_data['para1']
+    para2 = request_data['para2']
+    para3 = request_data['para3']
+    para4 = request_data['para4']
+    para5 = request_data['para5']
+    para6 = request_data['para6']
+    para7 = request_data['para7']
+    para8 = request_data['para8']
+    para9 = request_data['para9']
+    para10 = request_data['para10']
+    para11 = request_data['para11']
+    para12 = request_data['para12']
+    para13 = request_data['para13']
+    para14 = request_data['para14']
+    para15 = request_data['para15']
+    para16 = request_data['para16']
+    para17 = request_data['para17']
+    add_data(table_name, para1, para2, para3, para4, para5, para6, para7, para8, para9, para10, para11, para12, para13, para14, para15, para16, para17)
+    return 'add data successful'
 
-@app.route('/api/user/admin', methods=['POST'])
-def api_user_admin():
-    request_data = request.get_json() #requesting json
-    fname = request_data['firstname']
-    lname = request_data['lastname']
-    email = request_data['email']
-    code = request_data['code']
-    token = request_data['token']
 
+# for customer
+"""
+{
+    "table_name": "customer",
+    "para1": "fname", 
+    "para2": "lname",
+    "para3": "email",
+    "para4": "phonenum",
+    "para5": "bd",
+    "para6": "uniquecode",
+    "para7": "address",
+    "para8": "cus type id", #default 1 new
+    "para9": "country id", #default 185 usa
+    "para10": "discount have", #default 0
+    "para11": "state short", #default TX
+    "para12": "city",
+    "para13": "zip",
+    "para14": "cus statuss", #default 1 active
+    "para15": "promo status", #default 2 no
+    "para16": "discount used", #default 0 none
+    "para17": "registration date" #default todays date
+}
+"""
 
+#any other ones
+"""
+{
+    "table_name": "Contact_method",
+    "para1": "Mailing", 
+    "para2": "",
+    "para3": "",
+    "para4": "",
+    "para5": "",
+    "para6": "",
+    "para7": "",
+    "para8": "",
+    "para9": "",
+    "para10": "",
+    "para11": "",
+    "para12": "",
+    "para13": "",
+    "para14": "",
+    "para15": "",
+    "para16": "",
+    "para17": ""
+}
+"""
 
 
 
--- BackEndStarter.py
+++ BackEndStarter.py
@@ -15,39 +15,23 @@ cursor = conn.cursor()
 
 #########################################
 #DAN SECTION DO NOT CODE HERE 
-
-def employee_view():
-    employee_view = 'SELECT * FROM Employee'
-
-    try:
-        cursor.execute(employee_view)
-    except Error as e:
-        print (f"The error {e} has occur")
-    return cursor
-
-
-
-
-
-def customer_view():
-    customer_view = 'SELECT * FROM Customer'
-
-    try:
-        cursor.execute(customer_view)
-    except Error as e:
-        print (f"The error {e} has occur")
-    return cursor
+profile_info = ('Firstname:', 'Lastname:', 'Phone number:', 'Birth Date:', 'Unique code:', 'Address_line1:', 'Country:', 'Amount discount have:', 'State:', 'City:', 'Zipcode:', 'Amount discount used:')
 
 def get_customer_info(email):
-    get_info = f"SELECT * FROM Customer WHERE email = '{email}'"
+    new_return = {}
+    get_info = f"SELECT Fname, Lname, Phonenum, BD, Unique_code, Address_line1, CountryID, Amount_discount_have, StateShortened_name, City, Zipcode, Amount_discount_used FROM Customer WHERE email = '{email}'"
     try:
         cursor.execute(get_info)
         rows = cursor.fetchall()
     except Error as e:
         print (f"The error {e} has occur")
-    return rows
+    for x in range(11):    
+        new_return[profile_info[x]] = rows[0][x]
+    print(new_return)
+    return new_return
+    print(rows)
+#get_customer_info('yanahdi@gmail.com')
 
-#print(get_customer_info('yanahdi@gmail.com'))
 
 
 
@@ -258,23 +242,461 @@ def send_email(receiver_email, code):
     
     print("email sent")
 
-send_email("daannn445@gmail.com", "TEST")
-
-
+#send_email("daannn445@gmail.com", "TEST")
 ###########################
 
+def get_all_tables():
+    my_list = []
+    get = ("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'")
+    try:
+        cursor.execute(get)
+        rows = cursor.fetchall()
+        for item in rows:
+            if item[0] == 'sysdiagrams':
+                pass
+            else:
+                my_list.append(item[0])
+    except Error as e:
+        print (f"The error {e} has occur")
+    return my_list
+
+#print(get_all_tables())
+
+def view_table(name):
+    final_list = []
+    view = f'SELECT * FROM {name}'
+    try:
+        cursor.execute(view)
+        rows = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+    except Error as e:
+        print (f"The error {e} has occur")
+    for y in range(len(rows)):
+        my_dict = {}
+        for x in range(len(columns)):
+            my_dict[columns[x]] = rows[y][x]
+        final_list.append(my_dict)
+    return final_list
+
+#(view_table('Incident_list'))
+
+
+
+def add_data(table_name, para1, para2, para3, para4, para5, para6, para7, para8, para9, para10, para11, para12, para13, para14, para15, para16, para17):
+    if table_name == 'Contact_method':
+        add = ("INSERT INTO [Contact_method]\n"
+        "([Method])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+    if table_name == 'Country':
+        add = ("INSERT INTO [Country]\n"
+        "([Name])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+    if table_name == 'Customer':
+        add = ("INSERT INTO [Customer]\n"
+           "([Fname]\n"
+           ",[Lname]\n"
+           ",[Email]\n"
+           ",[Phonenum]\n"
+           ",[BD]\n"
+           ",[Unique_code]\n"
+           ",[Address_line1]\n"
+           ",[Customer_typeID]\n"
+           ",[CountryID]\n"
+           ",[Amount_discount_have]\n"
+           ",[StateShortened_name]\n"
+           ",[City]\n"
+           ",[Zipcode]\n"
+           ",[Customer_statusID]\n"
+           ",[Promo_signup_statusID]\n"
+           ",[Amount_discount_used]\n"
+           ",[Registration_date])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}'\n"
+           f",'{para4}'\n"
+           f",'{para5}'\n"
+           f",'{para6}'\n"
+           f",'{para7}'\n"
+           f",'{para8}'\n"
+           f",'{para9}'\n"
+           f",'{para10}'\n"
+           f",'{para11}'\n"
+           f",'{para12}'\n"
+           f",'{para13}'\n"
+           f",'{para14}'\n"
+           f",'{para15}'\n"
+           f",'{para16}'\n"
+           f",'{para17}')\n")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Customer_feedback':
+        add = ("INSERT INTO [Customer_feedback]\n"
+        "([CustomerID]\n"
+           ",[StoreID]\n"
+           ",[Feedback]\n"
+           ",[Customer_feedback_statusID])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}'\n"
+           f",'{para4}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+        
+    if table_name == 'Customer_feedback_status':
+        add = ("INSERT INTO [Customer_feedback_status]\n"
+        "([Status])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Customer_interaction':
+        add = ("INSERT INTO [Customer_interaction]\n"
+        "([ServiceID]\n"
+           ",[CustomerID]\n"
+           ",[Note]\n"
+           ",[Customer_interaction_statusID]\n"
+           ",[Used_discount])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}'\n"
+           f",'{para4}'\n"
+           f",'{para5}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Customer_interaction_status':
+        add = ("INSERT INTO [Customer_interaction_status]\n"
+        "([Name])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+    
+    if table_name == 'Customer_note':
+        add = ("INSERT INTO [Customer_note]\n"
+        "([Note]\n"
+           ",[CustomerID])\n"
+        "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+    
+    if table_name == 'Customer_social':
+        add = ("INSERT INTO [Customer_social]\n"
+        "([Social_typeID]\n"
+           ",[CustomerID]\n"
+           ",[Link])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Customer_social_type':
+        add = ("INSERT INTO [Customer_social_type]\n"
+        "([Type])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Customer_status':
+        add = ("INSERT INTO [Customer_status]\n"
+            "([Name]\n"
+           ",[Comment])\n"
+        "VALUES\n"
+            f"('{para1}'\n"
+            f",'{para2}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Customer_type':
+        add = ("INSERT INTO [Customer_type]\n"
+        "([Name])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    
+    if table_name == 'Employee':
+        add = ("INSERT INTO [Employee]\n"
+           "([Fname]\n"
+           ",[Lname]\n"
+           ",[Email]\n"
+           ",[Phonenum]\n"
+           ",[BD]\n"
+           ",[Change_discount]\n"
+           ",[Address_line1]\n"
+           ",[CountryID]\n"
+           ",[Employee_typeID]\n"
+           ",[StateShortened_name]\n"
+           ",[City]\n"
+           ",[Zipcode]\n"
+           ",[Employee_statusID])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}'\n"
+           f",'{para4}'\n"
+           f",'{para5}'\n"
+           f",'{para6}'\n"
+           f",'{para7}'\n"
+           f",'{para8}'\n"
+           f",'{para9}'\n"
+           f",'{para10}'\n"
+           f",'{para11}'\n"
+           f",'{para12}'\n"
+           f",'{para13}'\n")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Employee_note':
+        add = ("INSERT INTO [Employee_note]\n"
+            "([EmployeeID]\n"
+           ",[Note])\n"
+            "VALUES\n"
+            f"('{para1}'\n"
+            f",'{para2}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Employee_status':
+        add = ("INSERT INTO [Employee_status]\n"
+        "([Name])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Employee_type':
+        add = ("INSERT INTO [Employee_type]\n"
+        "([Name])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Incident_list':
+        add = ("INSERT INTO [Incident_list]\n"
+        "([Incident_typeID]\n"
+           ",[EmployeeID]\n"
+           ",[Note]\n"
+           ",[Other_employee]\n"
+           ",[CustomerID]\n"
+           ",[Cost])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}'\n"
+           f",'{para4}'\n"
+           f",'{para5}'\n"
+           f",'{para6}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Incident_type':
+        add = ("INSERT INTO [Incident_type]\n"
+        "([Name])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+
+    if table_name == 'Preferred_barber':
+        add = ("INSERT INTO [Preferred_barber]\n"
+            "([CustomerID]\n"
+           ",[EmployeeID])\n"
+            "VALUES\n"
+            f"('{para1}'\n"
+            f",'{para2}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Preferred_method':
+        add = ("INSERT INTO [Preferred_method]\n"
+            "([Contact_methodID]\n"
+           ",[CustomerID])\n"
+            "VALUES\n"
+            f"('{para1}'\n"
+            f",'{para2}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Promo_signup_status':
+        add = ("INSERT INTO [Promo_signup_status]\n"
+        "([Name])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'Service':
+        add = ("INSERT INTO [Service]\n"
+        "([Description]\n"
+           ",[Name]\n"
+           ",[Price])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+    if table_name == 'State':
+        add = ("INSERT INTO [State]\n"
+        "([Shortened_name]\n"
+           ",[Fullname]\n"
+           ",[CountryID])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+    
+    if table_name == 'Store':
+        add = ("INSERT INTO [Store]\n"
+           "([Store_name]\n"
+           ",[Address_line1]\n"
+           ",[CountryID]\n"
+           ",[StateShortened_name]\n"
+           ",[City]\n"
+           ",[Zipcode]\n"
+           ",[Phonenum])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}'\n"
+           f",'{para4}'\n"
+           f",'{para5}'\n"
+           f",'{para6}'\n"
+           f",'{para7}'\n")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+
+    if table_name == 'Token':
+        add = ("INSERT INTO [Token]\n"
+           "([Token]\n"
+           ",[Time_create]\n"
+           ",[Time_expire]\n"
+           ",[CustomerID]\n"
+           ",[Token_statusID])\n"
+     "VALUES\n"
+           f"('{para1}'\n"
+           f",'{para2}'\n"
+           f",'{para3}'\n"
+           f",'{para4}'\n"
+           f",'{para5}'\n")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
+
+
+    if table_name == 'Token_status':
+        add = ("INSERT INTO [Token_status]\n"
+        "([Name])\n"
+        "VALUES\n"
+        f"('{para1}')")
+        try:
+            cursor.execute(add)
+            conn.commit()
+        except Error as e:
+            print(f"The error '{e}' occurred")
 
 
 
 
-
-def email_message_signup(email):
-    #create sample email
-    #token_maker()
-    #send email to cus email with link w token
-    # https://realpython.com/handling-email-confirmation-in-flask/
-    pass
-
+#add_data('customer','','','','','','','','','','','','','','','','','')
 
 
 
